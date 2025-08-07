76bc3edbc0c74350fc7f7b632e794794f1f1772d
Aura-Lozano <auracristinalozanohoyos@gmail.com>
2025-08-07 15:39:46 -0500

    muchos cambios

--- codigo/app.js
+++ codigo/app.js
@@ -4,33 +4,10 @@ var app = angular.module("appProyecto", ["ngRoute"]);
 app.config(function ($routeProvider) {
   $routeProvider
 
-    .when('/historias', {
-      templateUrl: 'vistas/Historias.html',
-      controller: 'ControladorHistorias'
-    })
     .when('/ConsumoEnergetico', {
       templateUrl: 'vistas/ConsumoEnergetico.html',
       controller: 'ControladorConsumoEnergetico'
     })
-
-    .when('/Acerca', {
-      templateUrl: 'vistas/AcercaEnergia.html',
-      controller: 'ControladorAcerca'
-    })
-
-    .when('/GraficasInformativas', {
-      templateUrl: 'vistas/GraficasInformativas.html',
-      controller: 'ControladorGraficas'
-    })
-    .when('/InnovacionenColombia', {
-      templateUrl: 'vistas/InnovaciónenColombia.html',
-      controller: 'ControladorenColombia'
-    })
-    .when('/DesarrolloSostenible', {
-      templateUrl: 'vistas/DesarrolloSostenible.html',
-      controller: 'DesarrolloSostenible'
-    })
-
 
     .when("/radiacion", {
       templateUrl: "vistas/RadiacionSolar.html",
